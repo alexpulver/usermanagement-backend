40ab8da27cd889577bd2ea8c6c97c9675ffca7cb
Alex Pulver <apulver@amazon.com>
2021-08-09 15:53:03 +0300

    Update constants

--- constants.py
+++ constants.py
@@ -8,8 +8,8 @@ CDK_APP_NAME = "UserManagementBackend"
 # pylint: disable=line-too-long
 GITHUB_CONNECTION_ARN = "arn:aws:codestar-connections:eu-west-1:807650736403:connection/1f244295-871f-411f-afb1-e6ca987858b6"
 GITHUB_OWNER = "alexpulver"
-GITHUB_REPO = "aws-cdk-sam-chalice"
-GITHUB_TRUNK_BRANCH = "future"
+GITHUB_REPO = "usermanagement-backend"
+GITHUB_TRUNK_BRANCH = "main"
 
 CODEBUILD_INSTALL_COMMANDS = ["./scripts/install-deps.sh"]
 CODEBUILD_INSTALL_RUNTIME_VERSIONS = {"python": "3.7"}
